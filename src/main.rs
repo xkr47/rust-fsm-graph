d5ddc38ffa82e7fd8c301f0474e6792f609f6e64
Jonas Berlin <xkr47@outerspace.dyndns.org>
2020-11-28 10:15:37 +0200

    Cleanup, improve cli api

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Read;
 use std::process;
 
-use proc_macro2::{Delimiter, Spacing, TokenStream, TokenTree};
+use proc_macro2::TokenStream;
 use syn::{Item, parse2};
 
 use crate::parser::StateMachineDef;
@@ -17,7 +17,7 @@ fn main() {
     let filename = match (args.next(), args.next()) {
         (Some(filename), None) => filename,
         _ => {
-            eprintln!("Usage: dump-syntax path/to/filename.rs");
+            eprintln!("Usage: rust-fsm-graph path/to/filename.rs");
             process::exit(1);
         }
     };
@@ -29,8 +29,6 @@ fn main() {
 
     let syntax = syn::parse_file(&src).expect("Unable to parse file");
 
-
-    // Debug impl is available if Syn is built with "extra-traits" feature.
     syntax.items.into_iter()
         .filter_map(|item|
                     if let Item::Macro(item_macro) = item {
@@ -44,17 +42,23 @@ fn main() {
                         None
                     })
         .map(fsm_to_graphviz)
-        .for_each(|(name, str)| std::fs::write(format!("{}.dot", name), str).unwrap_or_else(|_| panic!("Failed to write {}.dot", name)))
+        .for_each(|(name, str)| {
+            let filename = format!("{}.dot", name);
+            std::fs::write(&filename, str).unwrap_or_else(|_| panic!("Failed to write {}", &filename));
+            println!("Wrote {}", filename);
+        });
 }
 
 fn fsm_to_graphviz(stream: TokenStream) -> (String, String) {
     let fsm = parse2::<StateMachineDef>(stream).expect("Failed to parse state machine definition");
     let name = fsm.name.to_string();
+
     let mut dot = format!(r#"digraph "graph" {{
   rankdir="LR";
   SM_init [label="", shape=point];
   SM_init -> "{}";
 "#, fsm.initial_state.to_string());
+
     fsm.transitions.into_iter()
         .flat_map(|from| {
             let from_state = from.initial_state.to_string();
@@ -67,76 +71,7 @@ fn fsm_to_graphviz(stream: TokenStream) -> (String, String) {
         })
         .for_each(|line| dot.push_str(&line));
 
-    /*
-    let mut iter = fsm.into_iter().peekable();
-    let name = get_ident(iter.next().unwrap());
-    let default_state = get_ident(get_single_token(get_group(iter.next().unwrap(), Delimiter::Parenthesis)));
-    let mut dot = format!(r#"digraph "graph" {{
-  SM_init [label="", shape=point]
-  SM_init -> {}  [style="solid"]
-"#, default_state);
-    while let Some(first) = iter.next() {
-        let from_state = get_ident(first);
-        expect_punct(iter.next().unwrap(), '=', Spacing::Joint);
-        expect_punct(iter.next().unwrap(), '>', Spacing::Alone);
-        let stream = get_group(iter.next().unwrap(), Delimiter::Brace);
-        let mut inner_iter = stream.into_iter();
-        while let Some(inner) = inner_iter.next() {
-            let input = get_ident(inner);
-            expect_punct(inner_iter.next().unwrap(), '=', Spacing::Joint);
-            expect_punct(inner_iter.next().unwrap(), '>', Spacing::Alone);
-
-
-            //  "idling" -> "listening" [label="audio received"];
-        }
-    }
-    */
     dot.push_str("}\n");
+
     (name, dot)
-}
-
-fn expect_punct(i: TokenTree, ch: char, spacing: Spacing) {
-    if let TokenTree::Punct(p) = i {
-        if p.as_char() != ch {
-            panic!("Expected '{}', got '{}'", ch, p.as_char());
-        }
-        if p.spacing() != spacing {
-            panic!("Wrong spacing after '{}'", ch);
-        }
-    } else {
-        panic!("Expected punctuation '{}', got '{:#?}'", ch, i);
-    }
-}
-
-fn get_single_token(ts: TokenStream) -> TokenTree {
-    let mut iter = ts.into_iter();
-    if let Some(token) = iter.next() {
-        if iter.next().is_none() {
-            token
-        } else {
-            panic!("Extra tokens in token stream");
-        }
-    } else {
-        panic!("Empty token stream");
-    }
-}
-
-fn get_group(i: TokenTree, delimiter: Delimiter) -> TokenStream {
-    if let TokenTree::Group(g) = i {
-        if g.delimiter() == delimiter {
-            g.stream()
-        } else {
-            panic!("Failed to parse ident");
-        }
-    } else {
-        panic!("Failed to parse {:?} group", delimiter);
-    }
-}
-
-fn get_ident(i: TokenTree) -> String {
-    if let TokenTree::Ident(s) = i {
-        s.to_string()
-    } else {
-        panic!("Failed to parse ident");
-    }
 }
